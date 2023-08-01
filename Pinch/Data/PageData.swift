c1ab44f7ce0568db1d908bfb01a1b18ecd129101
ek223gi <ek223gi@student.lnu.se>
2023-08-01 19:12:26 +0300

    Initial Commit

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Ege Kayihan on 1.08.2023.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
